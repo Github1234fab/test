609122b436a1343a9f9a2579794292d670ac8a06
Fabien Marceau <fabien.marceau@gmail.com>
2022-12-21 15:02:38 +0100

    Design Logement page

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -3,8 +3,10 @@ import "./Tag.css";
 
 const Tag = ({ tag }) => {
     return (
+        <div className='tag_container'>
         <div className='tag'>
             <p className='tag_txt'>{tag}</p>
+        </div>
         </div>
     );
 };
