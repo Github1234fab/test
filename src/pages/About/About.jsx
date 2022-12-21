adec8583a08660493be8c9933fc65712f3073449
Fabien Marceau <fabien.marceau@gmail.com>
2022-12-21 16:14:57 +0100

    pb collapse "générique pour design css

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,14 +2,25 @@ import React from 'react';
 import "./About.css";
 import Banner2 from '../../components/Banner2';
 import Collapse from '../../components/Collapse';
+// import DataLogements from "../../data/data.json";
 
 const About = () => {
-    return (
-        <div>
-          <Banner2 />
-          <Collapse />
+
+  return (
+    <>
+    <div>
+      <Banner2 />
+      </div>
+      <div className='about_collapses'>
+        <div className='about_anyCollapse'>
+        <Collapse className='about_collapseFiabilite' />
+        <Collapse className='about_collapseRespect' />
+        <Collapse className='about_collapseService' />
+        <Collapse className='about_collapseResponsabilite' />
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default About;
