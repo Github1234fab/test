c931d33e6e2572b73fea3b3d34f6946857232a11
Fabien Marceau <fabien.marceau@gmail.com>
2022-12-22 20:20:06 +0100

    Version desktop ok sauf étoiles

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,6 +7,7 @@ import AboutTxt from "../../data/AboutTxt.json"
 const About = () => {
   const text = AboutTxt.find((text)=>text);
   const {txt1, title1, txt2, title2, txt3, title3, txt4, title4 } = text;
+  
 
   return (
     <>
@@ -19,6 +20,7 @@ const About = () => {
           <Collapse className='about_collapseTxt2' description={txt2} titre={title2} />
           <Collapse className='about_collapseTxt3' description={txt3} titre={title3} />
           <Collapse className='about_collapseTxt4' description={txt4} titre={title4} /> 
+         
          
         </div>
       </div>
