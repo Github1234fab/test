8926482e92257869218470e94a90bd0d6ab85296
Fabien Marceau <fabien.marceau@gmail.com>
2023-01-03 16:24:54 +0100

    ok about page Collapse

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,13 +2,9 @@ import React from 'react';
 import "./About.css";
 import Banner2 from '../../components/Banner2';
 import Collapse from '../../components/Collapse';
-import AboutTxt from "../../data/AboutTxt.json"
+import AboutData from "../../data/AboutData.json"
 
-const About = () => {
-  const text = AboutTxt.find((text)=>text);
-  const {txt1, title1, txt2, title2, txt3, title3, txt4, title4 } = text;
-  
-
+const About = () =>{
   return (
     <>
       <div>
@@ -16,10 +12,7 @@ const About = () => {
       </div>
       <div className='about_collapses'>
         <div className='about_anyCollapse'>
-          <Collapse className='about_collapseTxt1' description={txt1} titre={title1} />
-          <Collapse className='about_collapseTxt2' description={txt2} titre={title2} />
-          <Collapse className='about_collapseTxt3' description={txt3} titre={title3} />
-          <Collapse className='about_collapseTxt4' description={txt4} titre={title4} /> 
+        {AboutData.map((data) => (<Collapse description={data.content} titre ={data.title} />))}
         </div>
       </div>
     </>
