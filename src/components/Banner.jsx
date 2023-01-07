5221f7247c1561106f646d5301e33764530425f8
Fabien Marceau <fabien.marceau@gmail.com>
2023-01-08 00:35:31 +0100

    Corrections suite soutenance

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,10 +4,8 @@ import Ocean from '../assets/IMG_banner.jpg';
 const Banner = () => {
     return (
         <div className="banner">
-            {/* <div className="banner_visuel"> */}
                 <img className="banner_img" src={Ocean} alt="Côte océan" />
                 <h1 className='banner_txt' >Chez vous, partout et ailleurs</h1>
-            {/* </div> */}
         </div>
     );
 };
